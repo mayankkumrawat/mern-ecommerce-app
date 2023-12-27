dc288f39368f13eed11d8d8797f0bd123f04c18c
Mayank Kumrawat <mkmayank1806@gmail.com>
2023-12-27 13:13:27 +0530

    initial commit

---
+++ client/src/data.js
@@ -0,0 +1,56 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://img.mensxp.com/media/content/2023/May/Main-Image---iStock_6468782f3daca.jpeg?w=750&h=625&cc=1',
+    title: 'SUMMER SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: 'https://i.ibb.co/DG69bQ4/2.png',
+    title: 'AUTUMN COLLECTION',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: 'https://i.ibb.co/cXFnLLV/3.png',
+    title: 'LOUNGEWEAR LOVE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'fbf0f4',
+  },
+]
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    title: 'SHIRT STYLE!',
+    cat: 'men',
+  },
+  {
+    id: 2,
+    img: 'https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    title: 'LOUNGEWEAR LOVE',
+    cat: 'shoes',
+  },
+  {
+    id: 3,
+    img: 'https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    title: 'LIGHT JACKETS',
+    cat: 'tshirts',
+  },
+  {
+    id: 4,
+    img: 'https://images.pexels.com/photos/4458554/pexels-photo-4458554.jpeg',
+    title: 'COZY CHIC',
+    cat: 'shoes',
+  },
+  {
+    id: 5,
+    img: 'https://images.pexels.com/photos/16794751/pexels-photo-16794751/free-photo-of-a-woman-with-a-tote-bag-on-a-street.jpeg',
+    title: 'BOHO BEAUTY',
+    cat: 'tshirts',
+  },
+]
